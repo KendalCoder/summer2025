eabd0c5c20f1e717fc959ffb38fb23ea80cea684
KendalCoder <kendalrc13@gmail.com>
2025-06-10 15:18:32 -0500

    Update main.py

--- Edge_Scheduler_Sim/main.py
+++ Edge_Scheduler_Sim/main.py
@@ -2,16 +2,3 @@
 # It initializes the scheduler and registers tasks to be executed.
 # Edge_Scheduler_Sim/main.py
 
-
-from scheduler import Scheduler
-from task_a import run_task_a
-from task_b import run_task_b
-from task_c import run_task_c
-
-scheduler = Scheduler()
-scheduler.register_task("Task A", run_task_a)
-scheduler.register_task("Task B", run_task_b)
-scheduler.register_task("Task C", run_task_c)
-
-scheduler.run_all()
-
